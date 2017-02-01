f28849a46713c55709ba8a3d1c5e7970eaa7653f
Navdeep Gill <navdeep@h2o.ai>
2017-02-01 14:29:26 -0800

    Automatically decipher Sparkling Water version based on Spark version

--- R/package.R
+++ R/package.R
@@ -1,22 +1,40 @@
 #' @importFrom utils capture.output browseURL
 #' @importFrom sparklyr spark_dependency register_extension invoke_static invoke spark_connection spark_dataframe sdf_register spark_context
 #' @importFrom h2o h2o.getFrame h2o.getId h2o.init
+#' @importFrom utils read.table
 NULL
 
-# define required spark packages
+# Define required spark packages
 spark_dependencies <- function(spark_version, scala_version, ...) {
+  
+  #Check if sw_version/sw_location is provided
   sw_version <- getOption("rsparkling.sparklingwater.version", default = NULL)
   sw_location <- getOption("rsparkling.sparklingwater.location", default = NULL)
   
+  #If sw_version & sw_location are not provided, then check the Spark installation
+  #If its Spark 2.0.*, then we fetch the latest Sparkling Water for Spark 2.0.*
+  #If its Spark 1.6.*, then we fetch the latest Sparkling Water for Spark 1.6.*
+  #If none of the above, then throw an exception
+  #Also provide adequate version of H2O for latest Sparkling Water
   if(is.null(sw_version) && is.null(sw_location)){
-    stop("
-    ---------------------------------------------------------------------------------------------------------------------------------------------
-    Sparkling Water version is missing and a custom path to a Sparkling Water jar is not set. 
-    Please choose a correct version using options, for example: options(rsparkling.sparklingwater.version = '1.6.7')
-    or provide a path to a custom Sparkling Water jar, for example: options(rsparkling.sparklingwater.location = 'path/to/sw.jar'
-    ---------------------------------------------------------------------------------------------------------------------------------------------")
+    if(as.package_version(spark_version)$major == "2"){
+      #Get latest Sparkling Water release for Spark 2.0.*
+      latest = read.table("http://s3.amazonaws.com/h2o-release/sparkling-water/rel-2.0/latest")
+      sw_version = sprintf("2.0.%s",latest)
+      message(sprintf("Spark version %s detected. Will call latest Sparkling Water version %s",spark_version,sw_version))
+      message('\nEnsure H2O version 3.10.1.2 is installed. To install simply call install_h2o(release_name = "rel-turnbull",release_number = "2")')
+    }else if(as.package_version(spark_version)$major == "1" && as.package_version(spark_version)$minor == "6" ){ #Assuming Spark 1.6
+      #Get latest Sparkling Water release for Spark 1.6.*
+      latest = read.table("http://s3.amazonaws.com/h2o-release/sparkling-water/rel-1.6/latest")
+      sw_version = sprintf("1.6.%s",latest) 
+      message(sprintf("Spark version %s detected. Will call latest Sparkling Water version %s",spark_version,sw_version))
+      message('\nEnsure H2O version 3.10.0.7 is installed. To install simply call install_h2o(release_name = "rel-turing",release_number = "7")')
+    }else{
+      stop("Spark installation 1.6.* or 2.0.* are not detected. Please install Spark 2.0.* or 1.6.*")
+    }
   }
   
+  #Is a path to a Sparkling Water jar provided?
   if((!is.null(sw_location) && (!is.null(sw_version)) || !is.null(sw_location))){
     spark_dependency(
       jars = c(sw_location)
