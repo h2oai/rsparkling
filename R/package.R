9354eb42502d8b3f442c991f997b04ba7d9183bc
Navdeep Gill <navdeep@h2o.ai>
2017-03-09 14:25:02 -0800

    packageVersion should be in new line

--- R/package.R
+++ R/package.R
@@ -1,7 +1,8 @@
 #' @importFrom utils capture.output browseURL
 #' @importFrom sparklyr spark_dependency register_extension invoke_static invoke spark_connection spark_dataframe sdf_register spark_context
 #' @importFrom h2o h2o.getFrame h2o.getId h2o.init
-#' @importFrom utils read.table packageVersion
+#' @importFrom utils read.table
+#' @importFrom utils packageVersion
 NULL
 
 # Define required spark packages
